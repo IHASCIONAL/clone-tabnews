d5189494fe1499c08bd2099a2e6ad685cf71c922
Ismael Henrique <104228083+IHASCIONAL@users.noreply.github.com>
2025-08-18 20:33:31 +0000

    Adiciona scripts de linting e formatação

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Uma frase impactante!!!</h1>
+  return <h1>Uma frase impactante!!!</h1>;
+}
+
+function teste() {
+  return <h1>Teste</h1>;
+}
+
+function teste2() {
+  return <h1>Teste 2</h1>;
 }
 
 export default Home;
